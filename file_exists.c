2cdd08e4744d74aef79b4aae24b2493b92bf10b7
Nakell <mwongelijudym@gmail.com>
2023-08-21 02:49:04 -0700

    update

--- file_exists.c
+++ file_exists.c
@@ -3,10 +3,16 @@
 /**
  * file_exists - This function checks if a file exists at a given path
  * @p: Path
- * @buff: This struct stores info about the file like size
  * Return: A boolean value. True if the file exists and false otherwise
  */
-bool file_exists(const char *p, struct stat *buff)
+bool file_exists(const char *p)
 {
-	return (stat(p, buff) == 0);
+	if (access(p, F_OK) == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
