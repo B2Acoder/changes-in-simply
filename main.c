4a849ecee77a838f66c77298c11cd78e1682903f
Nakell <mwongelijudym@gmail.com>
2023-08-21 02:48:19 -0700

    update

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "shell.h"
 
 /**
  * main - checks the code
@@ -6,8 +6,25 @@
  * Return: Always 0
  */
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	prompt_loop();
+	char command_input[1024];/*stores the actual user input*/
+	ssize_t read_size; /*keeps track of the number of chars read*/
+	if (argc == 1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			prompt_loop();
+		}
+		else
+		{
+			char command_input[1024];
+			ssize_t read_size;
+		}
+		while ((read_size = read(STDIN_FILENO, command_input, sizeof(command_input))) > 0)
+		{
+			write(STDOUT_FILENO, command_input, read_size);
+		}
+	}
 	return (0);
 }
