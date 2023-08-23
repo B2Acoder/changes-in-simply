8eed6fcc1e301eb8a512078401d0143cb3c4e5af
Muoti5 <Mbathamuoti19@students.ku.ac.ke>
2023-08-23 16:37:46 -0700

    PROMPT

--- execute.c
+++ execute.c
@@ -8,40 +8,43 @@
  */
 void execute_command(char *command_path)
 {
-    char **arguments = tokenize_commandargs(command_path);
+	char **arguments = tokenize_commandargs(command_path);
 
-    if (arguments == NULL || arguments[0] == NULL)
-    {
-        printf("</3: Empty command\n");
-        return;
-    }
+	if (arguments == NULL || arguments[0] == NULL)
+	{
+		printf("</3: Empty command\n");
+		return;
+	}
 
-    printf("Debug: Command to execute: %s\n", arguments[0]);
+	printf("Debug: Command to execute: %s\n", arguments[0]);
+	if (strcmp(arguments[0], "exit") == 0)
+	{
+		free_arguments(arguments);
+		shell_exit();
+	}
 
-    if (strcmp(arguments[0], "exit") == 0)
-    {
-	    free_arguments(arguments);
-	    shell_exit();
-    }
-    else if (strcmp(arguments[0], "env") == 0)
-    {
-        print_env();
-    }
-    else
-    {
-        char *executablepath = find_exe(arguments[0]);
+	else if (strcmp(arguments[0], "env") == 0)
+	{
+		print_env();
+	}
 
-        if (executablepath != NULL)
-        {
-            printf("Debug: Executable path to execute: %s\n", executablepath);
-            execute_executable(executablepath, arguments);
-            free(executablepath);
-        }
-        else
-        {
-            fprintf(stderr, "</3: %s: command not found\n", arguments[0]);
-        }
-    }
+	else
+	{
+		char *executablepath = find_exe(arguments[0]);
 
-    free_arguments(arguments);
+		if (executablepath != NULL)
+		{
+			printf("Debug: Executable path to execute: %s\n", executablepath);
+
+			execute_executable(executablepath, arguments);
+			free(executablepath);
+		}
+
+		else
+		{
+			fprintf(stderr, "</3: %s: command not found\n", arguments[0]);
+		}
+	}
+
+	free_arguments(arguments);
 }
